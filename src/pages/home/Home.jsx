6734a07cf89b52131d9274df22b3119700449a96
Esmer-Terishli <esmerterishli@mail.ru>
2023-12-08 17:33:42 +0400

    changing message

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,8 @@ import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
-import TopRated from "./topRated/topRated";
+import TopRated from "./topRated/TopRated"
+
 
 const Home = () => {
   return (
